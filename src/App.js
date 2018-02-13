6cb9b6b319435e3fce3e3a3a640ba8f19fdd2cc0
cfalcant <cfalcant@gmail.com>
2018-02-13 15:34:28 -0700

    full list of items rendering

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import React, {Component} from 'react';
 import CartHeader from './components/CartHeader'
 import CartFooter from './components/CartFooter'
 import CartItems from './components/CartItems'
+// import CartItem from './components/CartItem'
 
 let applicationstate = {
-  copyright: 2018
+  copyright: 2018,
+  items: [
+    { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 3.99 }, quantity: 1 },
+    { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 4.99 }, quantity: 2 },
+    { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 19.99 }, quantity: 1 },
+  ]
 }
+
 
 class App extends Component {
   render () {
   return (
     <div className="App">
       <CartHeader />
-      <CartItems />
+      <CartItems bigList={applicationstate.items} />
       <CartFooter copyrightyear={applicationstate.copyright} />
     </div>
   );
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import CartItem from "./CartItem"
 
-const CartItems = () => {
+
+// const CartItems = ({items}) => {
+//   return (
+//     <div className="collection">
+//     </div>
+//   )
+// }
+
+const CartItems = (props)=> {
+      let bigList = props.bigList.map(item => <CartItem key={item.id} item={item} />)
   return (
     <div className="container">
       <h1>Items</h1>
+      <div className="collection">
+        <div className="collection-item row grey lighten-3">
+          <div className="col s8">Product</div>
+          <div className="col s2">Price</div>
+          <div className="col s2">Quantity</div>
+        </div>
+          {bigList}
+      </div>
     </div>
   )
 }
