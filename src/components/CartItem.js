585f2a4906c6675746b1f9a042df899c5981c215
cfalcant <cfalcant@gmail.com>
2018-02-13 15:36:10 -0700

    full list of items rendering;correccted minor issue involving currency symbol

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,9 +3,9 @@ import React from 'react'
 const CartItem = ({item}) => (
   <div className="collection-item">
     <div className="row">
-      <div className="col s8">${item.product.name}</div>
+      <div className="col s8">{item.product.name}</div>
       <div className="col s2">${item.product.priceInCents}</div>
-      <div className="col s2">${item.quantity}</div>
+      <div className="col s2">{item.quantity}</div>
     </div>
   </div>
 )
